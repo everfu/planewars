83b180c6db007c242c6273287618399503a8e08c
伍拾柒 <o@efu.me>
2026-08-09 13:01:41 +0800

    feat(bullet): add EnemyBullet class for enemy fire

--- bullet.py
+++ bullet.py
@@ -1,4 +1,27 @@
 import pygame
+from random import *
+
+class EnemyBullet(pygame.sprite.Sprite):
+    def __init__(self,bg_size,position,speed):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image=pygame.image.load('images/bullet1.png').convert_alpha()
+        self.rect=self.image.get_rect()
+        self.rect.centerx,self.rect.top=position
+        self.speed=speed
+        self.height=bg_size[1]
+        self.active=False
+        self.mask=pygame.mask.from_surface(self.image)
+
+    def move(self):
+        self.rect.top +=self.speed
+
+        if self.rect.top>self.height:
+            self.active=False
+
+    def reset(self,position):
+        self.rect.centerx,self.rect.top=position
+        self.active=True
 
 class Bullet1(pygame.sprite.Sprite):
     def __init__(self,position):
